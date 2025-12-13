807979ba8d3af075fcd90d509e3314df860adf54
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-13 18:54:22 +0530

    final url added

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://gym-management-frontend-five.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,

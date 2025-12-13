3589e13d69efa0523b9b547926684801904a52fb
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-12-13 16:30:02 +0530

    update lead done

--- src/leads/dto/update-lead.dto.ts
+++ src/leads/dto/update-lead.dto.ts
@@ -1,4 +1,36 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateLeadDto } from './create-lead.dto';
+import {
+  IsString,
+  IsOptional,
+  IsDateString,
+  IsArray,
+  ValidateNested,
+  IsInt,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateLeadDto extends PartialType(CreateLeadDto) {}
+class UpdateLeadNoteDto {
+  @IsOptional()
+  @IsInt()
+  id?: number;
+
+  @IsOptional()
+  @IsInt()
+  leadId?: number;
+
+  @IsOptional()
+  @IsDateString()
+  date?: string;
+
+  @IsString()
+  note: string;
+}
+
+export class UpdateLeadDto extends PartialType(CreateLeadDto) {
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => UpdateLeadNoteDto)
+  notes?: UpdateLeadNoteDto[];
+}
